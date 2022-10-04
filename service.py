79f004575c1feb11460733e93c9124d486507fa8
J. Bukhari <jb@Js-MacBook-Air.local>
2022-10-04 12:48:28 -0400

    refactor

--- service.py
+++ service.py
@@ -1,37 +1,30 @@
 # This Lambda function performs a dlx auth merge. 
-# Event parameters: gaining_id (int), losing_id (int)
-# invocation: `<boto3 client>.invoke(FunctionName='dlx-auth-merge', payload='{"gaining_id": 1, "losing_id: 2"}')`
+# Event parameters: gaining_id (int), losing_id (int), user (str)
+# Invocation: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lambda.html#Lambda.Client.invoke
+# 
+# <boto3 client>.invoke(
+#   FunctionName='dlx-auth-merge', 
+#   Payload=b'{"gaining_id": 1, "losing_id": 2, "user": "username"}'
+# )
 
-from time import time
-from boto3 import client as botoclient
-from dlx import DB
-from dlx.marc import Auth
+from unittest.mock import NonCallableMagicMock
+from boto3 import client
+from dlx.scripts import auth_merge
 
 def handler(event, context):
-    ssm = botoclient('ssm', region_name='us-east-1')
+    ssm = client('ssm', region_name='us-east-1')
 
-    try:
-        # dev db for now
-        #connection_string = ssm.get_parameter(Name='connect-string')['Parameter']['Value']
-        connection_string = ssm.get_parameter(Name='dev-dlx-connect-string')['Parameter']['Value']
-    except:
-        raise Exception('Unable to get AWS SSM parameter')
+    # dev db for now
+    #connection_string = ssm.get_parameter(Name='connect-string')['Parameter']['Value']
+    connection_string = ssm.get_parameter(Name='dev-dlx-connect-string')['Parameter']['Value']
 
-    DB.connect(connection_string)
-    print(f'Merging auth {event.losing_id} into {event.gaining_id}')
-    started = time()
-    gaining_auth = Auth.from_id(event.gaining_id)
-    
-    if gaining_auth is None:
-        raise Exception(f'Gaining record {event.gaining_id} not found')
+    if connection_string is None:
+        raise Exception('Unable to find DB connection credentials in AWS SSM')
 
-    losing_auth = Auth.from_id(event.losing_id)
-
-    if losing_auth is None:
-        raise Exception(f'Losing record {event.losing_id} not found')
-
-    print(f'{losing_auth.in_use(usage_type="bib")} bibs and {losing_auth.in_use(usage_type="auth")} auths will be updated')
-
-    # perform merge
-    gaining_auth.merge(losing_auth)
-    print(f'Finished in {started - time()} seconds')
+    auth_merge.run(
+        connect=connection_string,
+        gaining_id=event.gaining_id,
+        losing_id=event.losing_id,
+        user=event.user,
+        skip_prompt=True
+    )
