dffb10184cfcb82121098d80447286dd3e55b406
J. Bukhari <jb@Js-MacBook-Air.local>
2022-10-04 15:48:29 -0400

    update param handling

--- service.py
+++ service.py
@@ -2,28 +2,21 @@
 # Event parameters: gaining_id (int), losing_id (int), user (str)
 # Invocation: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lambda.html#Lambda.Client.invoke
 # 
-# <boto3 client>.invoke(
+# boto3.client('lambda').invoke(
 #   FunctionName='dlx-auth-merge', 
-#   Payload=b'{"gaining_id": 1, "losing_id": 2, "user": "username"}'
+#   Payload=b'{"connect": <mongodb connection string>, gaining_id": 1, "losing_id": 2, "user": "username"}'
 # )
 
-from boto3 import client
 from dlx.scripts import auth_merge
 
 def handler(event, context):
-    ssm = client('ssm', region_name='us-east-1')
-
-    # dev db for now
-    #connection_string = ssm.get_parameter(Name='connect-string')['Parameter']['Value']
-    connection_string = ssm.get_parameter(Name='dev-dlx-connect-string')['Parameter']['Value']
-
-    if connection_string is None:
-        raise Exception('Unable to find DB connection credentials in AWS SSM')
+    for _ in ['connect', 'gaining_id', 'losing_id', 'user']:
+        if _ not in event.keys(): raise Exception('event dict missing one or more required keys: connect, gaining_id, losing_id, user')
 
     auth_merge.run(
-        connect=connection_string,
-        gaining_id=event.gaining_id,
-        losing_id=event.losing_id,
-        user=event.user,
+        connect=event['connection_string'],
+        gaining_id=event['gaining_id'],
+        losing_id=event['losing_id'],
+        user=event['user'],
         skip_prompt=True
     )
